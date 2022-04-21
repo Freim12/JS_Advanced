09aa2063f975ccab8462cbb421cbaebb6fa25912
Freim <freimblack1@gmail.com>
2022-04-21 20:41:01 +0300

    Synchronized with git project

---
+++ Практика/practica4/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <calculation-number
+    v-model:current-value="currentValue" 
+    />
+    <calculation-number 
+    :counterValue="currentValue"
+    @change="currentValue+=$event"
+    />
+  </div>
+</template>
+
+<script>
+import CalculationNumber from './components/Calculation/'
+
+export default {
+  name: 'App',
+  components: {
+    CalculationNumber
+  },
+  data() {
+    return {
+      currentValue: 0
+    }
+  },
+  methods: {
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Практика/practica4/src/components/Calculation/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div>
+            {{currentValue}}
+        </div>
+        <div>
+            <button @click="onChange(-1)">-1</button>
+            <button @click="onChange(+1)">+1</button>
+        </div>
+        <div>
+            {{numberType}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CalculationNumber',
+        props: {
+            currentValue: {
+                type: Number,
+                default: 0
+            },
+        },
+        computed: {
+            numberType() {
+                if(this.currentValue>0) return 'Positive'
+                if(this.currentValue<0) return 'Negative'
+                else return 'Zero'
+            },
+        },
+        methods: {
+            onChange(increment) {
+                this.$emit("update:currentValue", this.currentValue+increment)
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
