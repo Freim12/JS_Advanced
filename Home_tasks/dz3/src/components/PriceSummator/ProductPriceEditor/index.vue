975456bdf1034f0ac65b12685b7ac12a09996c9b
UkrAndy <brila_andrij@ukr.net>
2022-04-27 17:13:52 +0300

    props не змінювати!!!

--- Home_tasks/dz3/src/components/PriceSummator/ProductPriceEditor/index.vue
+++ Home_tasks/dz3/src/components/PriceSummator/ProductPriceEditor/index.vue
@@ -2,7 +2,7 @@
     <div class="prod">
         <div v-for="(product,index) in productPrice" :key="index">
                 <h2>{{product.title}}</h2>
-                Price: <input type="number" v-model="product.price"><br>
+                Price: <input type="number" v-model="product.price"><br> Так робити не дуже добре. Вам передали масив чезе props, а ви його змінюєте
                 Count: <input type="number" v-model="product.count"><br>
                 Tax: <input type="number" v-model="product.tax" disabled>
         </div>
