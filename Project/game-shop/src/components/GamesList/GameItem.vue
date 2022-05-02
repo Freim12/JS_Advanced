ca605461deba9fe06d121032267d5734bfa0b855
Freim <freimblack1@gmail.com>
2022-05-02 14:16:41 +0300

    GameItem added

---
+++ Project/game-shop/src/components/GamesList/GameItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="game-item">
+        <div><img :src="gameItem.url"></div>
+        <div><span>{{gameItem.title}}</span></div>
+        <div><span>{{gameItem.price}} грн</span></div>
+        <div><button @click="$emit('buy')">Купити</button></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GameItem",
+        props: {
+            gameItem: {
+                type: Object,
+                default: () => {}
+            },
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+.game-item{
+}
+</style>
---
+++ Project/game-shop/src/components/GamesList/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <game-item 
+        v-for="gameItem in gamesList" 
+        :key="gameItem.id"
+        :gameItem="gameItem"
+        />
+    </div>
+</template>
+
+<script>
+import GameItem from "./GameItem.vue";
+// eslint-disable-next-line
+import { v4 as uuidv4 } from "uuid";
+    export default {
+        name: "GamesList",
+        components: {
+            GameItem,
+        },
+        data() {
+            return {
+                gamesList: [
+                    {
+                        id: uuidv4(),
+                        url: "https://game-shop.com.ua/prodimages/25445/thumbs/119123_300_300.jpg",
+                        title: "Kings Bounty II Day One Edition Xbox Series X/XboxOne Русская версия",
+                        price: 2058
+                    }
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
