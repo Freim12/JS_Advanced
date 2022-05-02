d534e2c82e0e72b80dded5ed0ef283c84dc280b1
Freim <freimblack1@gmail.com>
2022-05-02 16:05:55 +0300

    GameItem fixed

--- Project/game-shop/src/components/GamesList/GameItem.vue
+++ Project/game-shop/src/components/GamesList/GameItem.vue
@@ -30,14 +30,7 @@
     padding: 15px;
 }
 .game-item-block{
-    width: 23.9%;
-    box-shadow: none;
-    border: none;
-    margin: 0;
-    min-height: 250px;
-    position: relative;
-    padding: 6px;
-    margin-bottom: 10px;
+    width: 25%;
     overflow: hidden;
 }
 .img{
@@ -45,8 +38,6 @@
     margin: 0;
     text-align: center;
     overflow: hidden;
-    line-height: 0;
-    padding: 0 0 30px 0;
     border-bottom: 1px solid #f2f2f2;
 }
 .img img{
